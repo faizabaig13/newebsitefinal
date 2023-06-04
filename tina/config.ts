0c7807e0cd2cbc65be318b55d83af30c01522834
faiza <82286244+faiza255@users.noreply.github.com>
2023-06-04 18:26:22 +0500

    new commit

---
+++ tina/config.ts
@@ -0,0 +1,49 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "master";
+
+export default defineConfig({
+  branch,
+  clientId: '5189de1c-f7d4-453d-a77f-a92c6810e590', // Get this from tina.io
+  token: 'faecaa6df6a1e4f99d77ff3a9feb6a21c935c4dc', // Get this from tina.io
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+        ui: {
+          // This is an DEMO router. You can remove this to fit your site
+          router: ({ document }) => `/demo/blog/${document._sys.filename}`,
+        },
+      },
+    ],
+  },
+});
