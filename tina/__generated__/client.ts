4bbbcbdf546cfc298e335ec9d393ad3a92922bc6
faiza <82286244+faiza255@users.noreply.github.com>
2023-06-04 19:33:22 +0500

    Commit messagenewnewnre

---
+++ tina/__generated__/client.ts
@@ -0,0 +1,5 @@
+import { createClient } from "tinacms/dist/client";
+import { queries } from "./types";
+export const client = createClient({ url: 'https://content.tinajs.io/1.4/content/5189de1c-f7d4-453d-a77f-a92c6810e590/github/master', token: 'faecaa6df6a1e4f99d77ff3a9feb6a21c935c4dc', queries });
+export default client;
+  
